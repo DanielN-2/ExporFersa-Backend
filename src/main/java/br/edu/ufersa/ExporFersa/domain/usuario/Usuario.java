b2b1f1f3d99e09e3f6dff78964c4fb110b63081d
ovictorp <vpinto00056@gmail.com>
2026-09-08 10:38:04 -0300

    Feat. usuario entity done

--- src/main/java/br/edu/ufersa/ExporFersa/domain/usuario/Usuario.java
+++ src/main/java/br/edu/ufersa/ExporFersa/domain/usuario/Usuario.java
@@ -1,16 +1,48 @@
 package br.edu.ufersa.ExporFersa.domain.usuario;
 
+import br.edu.ufersa.ExporFersa.domain.projeto.Projeto;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table(name = "usuario")
-@Data
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @ManyToMany
+    @JoinTable(
+            name = "usuario_likes",
+            joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "projeto_id")
+    )
+    private List<Projeto> likes = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "usuario_projetos",
+            joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "projeto_id")
+    )
+    private List<Projeto> projetosQueFacoParte = new ArrayList<>();
 }
