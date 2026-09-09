7e7ce3afac6be5bad20c4d84031312febd2f5570
RickM19 <riquelmo14br@gmail.com>
2026-09-09 20:04:06 -0300

    feat: add project controllers and dtos

---
+++ src/main/java/br/edu/ufersa/ExporFersa/API/projeto/dtos/ProjetoResponse.java
@@ -0,0 +1,34 @@
+package br.edu.ufersa.ExporFersa.API.projeto.dtos;
+
+import java.util.List;
+
+public record ProjetoResponse(
+        Long id,
+        String nomeProjeto,
+        List<String> autores,
+        String videoURL,
+        String resumo,
+        String descricao
+) {
+    public ProjetoResponse {
+        if(id == null)
+            throw new IllegalArgumentException("O ID é obrigatório!");
+        if(nomeProjeto == null || nomeProjeto.isBlank() )
+            throw new IllegalArgumentException("O nome do projeto é obrigatório");
+
+        if (autores.stream().anyMatch(autor -> autor == null || autor.isBlank())) {
+            throw new IllegalArgumentException("O nome dos autores não pode ser vazio!");
+        }
+        if (videoURL == null || videoURL.isBlank()) {
+            throw new IllegalArgumentException("A URL do vídeo é obrigatória!");
+        }
+
+        if (resumo == null || resumo.isBlank()) {
+            throw new IllegalArgumentException("O resumo é obrigatório!");
+        }
+
+        if (descricao == null || descricao.isBlank()) {
+            throw new IllegalArgumentException("A descrição é obrigatória!");
+        }
+    }
+}
--- src/main/java/br/edu/ufersa/ExporFersa/API/usuario/UsuarioController.java
+++ src/main/java/br/edu/ufersa/ExporFersa/API/usuario/UsuarioController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/usuario")
+@RequestMapping("/api/v1/usuario")
 public class UsuarioController {
 
     @PostMapping
