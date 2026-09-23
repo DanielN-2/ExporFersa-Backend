514448872f399edcef0372a8e23cc7d09d9cca5d
Daniel <danielnoronha57@gmail.com>
2026-09-23 20:44:42 -0300

    feat: EventResponseDTO canonical constructor definition

--- src/main/java/br/edu/ufersa/ExporFersa/ExporFersaAPI/event/dtos/EventResponseDTO.java
+++ src/main/java/br/edu/ufersa/ExporFersa/ExporFersaAPI/event/dtos/EventResponseDTO.java
@@ -15,5 +15,29 @@ public record EventResponseDTO(
     OpeningHours hours,
     List<String> imagesURLs
 ) {
+    public EventResponseDTO {
+        if (id == null) {
+            throw new IllegalArgumentException("O campo id não pode ser nulo.");
+        }
 
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("O campo nome não pode estar vazio.");
+        }
+
+        if (category == null) {
+            throw new IllegalArgumentException("O campo categoria do evento não pode ser nulo.");
+        }
+
+        if (dates == null) {
+            throw new IllegalArgumentException("O campo dias de operação não pode ser nulo.");
+        }
+
+        if (hours == null) {
+            throw new IllegalArgumentException("O campo horario de funcionamento não pode ser nulo.");
+        }
+
+        if (imagesURLs == null) {
+            throw new IllegalArgumentException("A lista de imagens não pode ser nula.");
+        }
+    }
 }
