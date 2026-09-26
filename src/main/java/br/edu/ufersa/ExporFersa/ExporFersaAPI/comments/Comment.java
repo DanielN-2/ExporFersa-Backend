c198d5c17f0bfb0c02d99010c4e92d5859de86df
Kaio Silva <kaiofernando05cont@gmail.com>
2026-09-26 13:37:18 -0300

    refactor: changes comment entity

--- src/main/java/br/edu/ufersa/ExporFersa/ExporFersaAPI/comments/Comment.java
+++ src/main/java/br/edu/ufersa/ExporFersa/ExporFersaAPI/comments/Comment.java
@@ -1,8 +1,10 @@
 package br.edu.ufersa.ExporFersa.ExporFersaAPI.comments;
 
-import java.math.BigInteger;
+import java.util.Objects;
 import java.util.UUID;
 
+import br.edu.ufersa.ExporFersa.ExporFersaAPI.auth.User;
+import br.edu.ufersa.ExporFersa.ExporFersaAPI.project.Project;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -19,22 +21,22 @@ public class Comment {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private BigInteger id;
+    private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "project_id")
-    private UUID projectId;
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private UUID userId;
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @Column(nullable = false, length = 300)
     private String commentMessage;
 
     protected Comment() {}
 
-    public Comment(UUID projectId, UUID userId, String commentMessage) {
+    public Comment(Long projectId, UUID userId, String commentMessage) {
         if(commentMessage == null || commentMessage.isBlank()) {
             throw new IllegalArgumentException("O comentário não pode estar estar vazio!");
         }
@@ -43,8 +45,59 @@ public class Comment {
         }
     }
 
-    public BigInteger getId() { return id; }
-    public UUID getProjectId() { return projectId; }
-    public UUID getUserId() { return userId; }
+    public Comment(Project project, User user, String commentMessage) {
+        this.project = project;
+        this.user = user;
+        this.commentMessage = commentMessage;
+    }
+
+    public Comment(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(project, user, commentMessage);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Comment other = (Comment) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (project == null) {
+            if (other.project != null)
+                return false;
+        } else if (!project.equals(other.project))
+            return false;
+        if (user == null) {
+            if (other.user != null)
+                return false;
+        } else if (!user.equals(other.user))
+            return false;
+        if (commentMessage == null) {
+            if (other.commentMessage != null)
+                return false;
+        } else if (!commentMessage.equals(other.commentMessage))
+            return false;
+        return true;
+    }
+
+    public Long getId() { return id; }
+    public Project getProject() { return project; }
+    public User getUser() { return user; }
     public String getCommentMessage() { return commentMessage; }
+
+    public void setCommentMessage(String commentMessage) {
+        this.commentMessage = commentMessage;
+    }
 }
